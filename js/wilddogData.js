8333280c6b3cf5170e24cdb408c13e222a39d3c4
ECIZEP <ecizep@gmail.com>
2017-01-01 14:55:27 +0800

    chat module finished
    some UI of social module need to be modified

---
+++ js/wilddogData.js
@@ -0,0 +1,34 @@
+var wilddogData = {
+	config:{
+		authDomain: "shadow.wilddog.com",
+		syncURL: "https://shadow.wilddogio.com" //输入节点 URL
+	},
+	wirteData: function(path,key,value){
+		wilddog.initializeApp(this.config);
+		var ref = wilddog.sync().ref(path);
+		ref.child(key).set(value);
+	},
+	getData: function(path,callback){
+		if(typeof callback != "function"){
+			console.log("回调函数有误");
+		}
+		wilddog.initializeApp(this.config);
+		var ref = wilddog.sync().ref(path);
+		ref.once("value",function(value){
+			var data = value.val();
+			callback(data);
+		});
+	},
+	getDataOnChildAdded: function(path,callback){
+		if(typeof callback != "function"){
+			console.log("回调函数有误");
+		}
+		wilddog.initializeApp(this.config);
+		var ref = wilddog.sync().ref(path);
+		ref.on("child_added",function(value){
+			var data = value.val();
+			var key = value.key();
+			callback(data,key);
+		});
+	}
+}
